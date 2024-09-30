c180749035d8b9e42c500d2862387014e3283efd
Kauã Tiezzi <42079711+kauatiezzi@users.noreply.github.com>
2024-09-30 13:09:55 -0300

    Create 1.c

---
+++ 1.c
@@ -0,0 +1,12 @@
+//1 - 
+
+void main() {
+    int INDICE = 13, SOMA = 0, K=0;
+
+    while(K<INDICE) {
+        K = K + 1;
+        SOMA = SOMA + K;
+    }
+
+    printf("O valor de soma é: %d\n", SOMA); // O valor de soma é: 91
+}
